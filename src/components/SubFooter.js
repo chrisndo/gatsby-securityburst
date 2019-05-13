381f50b95ed0c8d6428ce2188c5d486ed17fd7fc
chrisndo <50555471+chrisndo@users.noreply.github.com>
2019-05-13 19:46:55 -0400

    Update SubFooter.js

--- src/components/SubFooter.js
+++ src/components/SubFooter.js
@@ -22,7 +22,7 @@ const SubFooter = props => (
             </ul>
             <ul>
               <li>
-                <a href="https://www.zerostatic.io">www.zerostatic.io</a>
+                <a href="https://www.securityburst.com">www.securityburst.com</a>
               </li>
             </ul>
           </div>
