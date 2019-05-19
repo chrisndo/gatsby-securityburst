c7afb6b0c1ad8f0044e40c10040163b9812d13dc
chrisndo <50555471+chrisndo@users.noreply.github.com>
2019-05-19 11:14:21 -0400

    Update index.js
    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ const Home = (props) => {
       </Helmet>
       <div className="intro pb-4">
         <div className="container">
-          <h1>SECURITY BURST</h1>
+          <h1>SECURITY BURST!</h1>
           <p>
             Helping to ensure that your infrastrucutre is secure.  Offering multiple services catered to meet your specific needs.
             Contact us for more information and a free consultation.
